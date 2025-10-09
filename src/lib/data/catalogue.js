d0e06cbab542c0a57870c52c66879c7504371d5b
SidneyRamphalile <72583663+SidneyRamphalile@users.noreply.github.com>
2025-10-09 10:08:17 +0200

    Tombstone Prices Update

--- src/lib/data/catalogue.js
+++ src/lib/data/catalogue.js
@@ -4,441 +4,441 @@ export const catalogue = [
 		id: 'KT001',
 		name: 'KT001',
 		description: 'Classic granite tombstone.',
-		price: 4500,
+		price: 35000,
 		image: '/images/products/KT001.jpg'
 	},
 	{
 		id: 'KT002',
 		name: 'KT002',
 		description: 'Elegant marble headstone.',
-		price: 4600,
+		price: 40000,
 		image: '/images/products/KT002.jpg'
 	},
 	{
 		id: 'KT003',
 		name: 'KT003',
 		description: 'Premium black granite tombstone.',
-		price: 4700,
+		price: 45000,
 		image: '/images/products/KT003.jpg'
 	},
 	{
 		id: 'KT004',
 		name: 'KT004',
 		description: 'Timeless design with polished finish.',
-		price: 4800,
+		price: 40000,
 		image: '/images/products/KT004.jpg'
 	},
 	{
 		id: 'KT005',
 		name: 'KT005',
 		description: 'Family tombstone for generations.',
-		price: 4900,
+		price: 40000,
 		image: '/images/products/KT005.jpg'
 	},
 	{
 		id: 'KT006',
 		name: 'KT006',
 		description: 'Elegant upright memorial.',
-		price: 5000,
+		price: 35000,
 		image: '/images/products/KT006.jpg'
 	},
 	{
 		id: 'KT007',
 		name: 'KT007',
 		description: 'Angel carving for remembrance.',
-		price: 5100,
+		price: 40000,
 		image: '/images/products/KT007.jpg'
 	},
 	{
 		id: 'KT008',
 		name: 'KT008',
 		description: 'Heart-shaped tombstone for loved ones.',
-		price: 5200,
+		price: 40000,
 		image: '/images/products/KT008.jpg'
 	},
 	{
 		id: 'KT009',
 		name: 'KT009',
 		description: 'Granite headstone with polished finish.',
-		price: 5300,
+		price: 40000,
 		image: '/images/products/KT009.jpg'
 	},
 	{
 		id: 'KT010',
 		name: 'KT010',
 		description: 'Classic upright tombstone.',
-		price: 5400,
+		price: 35000,
 		image: '/images/products/KT010.jpg'
 	},
 	{
 		id: 'KT011',
 		name: 'KT011',
 		description: 'Elegant memorial with engraving.',
-		price: 5500,
+		price: 40000,
 		image: '/images/products/KT011.jpg'
 	},
 	{
 		id: 'KT012',
 		name: 'KT012',
 		description: 'Double tombstone for families.',
-		price: 5600,
+		price: 45000,
 		image: '/images/products/KT012.jpg'
 	},
 	{
 		id: 'KT013',
 		name: 'KT013',
 		description: 'Polished granite tombstone.',
-		price: 5700,
+		price: 45000,
 		image: '/images/products/KT013.jpg'
 	},
 	{
 		id: 'KT014',
 		name: 'KT014',
 		description: 'White marble headstone.',
-		price: 5800,
+		price: 55000,
 		image: '/images/products/KT014.jpg'
 	},
 	{
 		id: 'KT015',
 		name: 'KT015',
 		description: 'Black granite tombstone with inscription.',
-		price: 5900,
+		price: 55000,
 		image: '/images/products/KT015.jpg'
 	},
 	{
 		id: 'KT016',
 		name: 'KT016',
 		description: 'Elegant upright memorial stone.',
-		price: 6000,
+		price: 40000,
 		image: '/images/products/KT016.jpg'
 	},
 	{
 		id: 'KT017',
 		name: 'KT017',
 		description: 'Family tombstone with engraving.',
-		price: 6100,
+		price: 45000,
 		image: '/images/products/KT017.jpg'
 	},
 	{
 		id: 'KT018',
 		name: 'KT018',
 		description: 'Heart-shaped granite tombstone.',
-		price: 6200,
+		price: 27000,
 		image: '/images/products/KT018.jpg'
 	},
 	{
 		id: 'KT019',
 		name: 'KT019',
 		description: 'Polished granite memorial.',
-		price: 6300,
+		price: 45000,
 		image: '/images/products/KT019.jpg'
 	},
 	{
 		id: 'KT020',
 		name: 'KT020',
 		description: 'Classic tombstone design.',
-		price: 6400,
+		price: 45000,
 		image: '/images/products/KT020.jpg'
 	},
 	{
 		id: 'KT021',
 		name: 'KT021',
 		description: 'Marble upright tombstone.',
-		price: 6500,
+		price: 45000,
 		image: '/images/products/KT021.jpg'
 	},
 	{
 		id: 'KT022',
 		name: 'KT022',
 		description: 'Black granite memorial stone.',
-		price: 6600,
+		price: 27000,
 		image: '/images/products/KT022.jpg'
 	},
 	{
 		id: 'KT023',
 		name: 'KT023',
 		description: 'Family memorial tombstone.',
-		price: 6700,
+		price: 33000,
 		image: '/images/products/KT023.jpg'
 	},
 	{
 		id: 'KT024',
 		name: 'KT024',
 		description: 'Elegant engraved tombstone.',
-		price: 6800,
+		price: 40000,
 		image: '/images/products/KT024.jpg'
 	},
 	{
 		id: 'KT025',
 		name: 'KT025',
 		description: 'Polished upright tombstone.',
-		price: 6900,
+		price: 40000,
 		image: '/images/products/KT025.jpg'
 	},
 	{
 		id: 'KT026',
 		name: 'KT026',
 		description: 'Heart-shaped memorial.',
-		price: 7000,
+		price: 35000,
 		image: '/images/products/KT026.jpg'
 	},
 	{
 		id: 'KT027',
 		name: 'KT027',
 		description: 'Classic granite design.',
-		price: 7100,
+		price: 30000,
 		image: '/images/products/KT027.jpg'
 	},
 	{
 		id: 'KT028',
 		name: 'KT028',
 		description: 'White marble memorial.',
-		price: 7200,
+		price: 35000,
 		image: '/images/products/KT028.jpg'
 	},
 	{
 		id: 'KT029',
 		name: 'KT029',
 		description: 'Black granite double tombstone.',
-		price: 7300,
+		price: 75000,
 		image: '/images/products/KT029.jpg'
 	},
 	{
 		id: 'KT030',
 		name: 'KT030',
 		description: 'Elegant upright tombstone.',
-		price: 7400,
+		price: 75000,
 		image: '/images/products/KT030.jpg'
 	},
 	{
 		id: 'KT031',
 		name: 'KT031',
 		description: 'Family memorial stone.',
-		price: 7500,
+		price: 75000,
 		image: '/images/products/KT031.jpg'
 	},
 	{
 		id: 'KT032',
 		name: 'KT032',
 		description: 'Classic engraved tombstone.',
-		price: 7600,
+		price: 75000,
 		image: '/images/products/KT032.jpg'
 	},
 	{
 		id: 'KT033',
 		name: 'KT033',
 		description: 'Polished granite upright.',
-		price: 7700,
+		price: 35000,
 		image: '/images/products/KT033.jpg'
 	},
 	{
 		id: 'KT034',
 		name: 'KT034',
 		description: 'Heart-shaped memorial stone.',
-		price: 7800,
+		price: 40000,
 		image: '/images/products/KT034.jpg'
 	},
 	{
 		id: 'KT035',
 		name: 'KT035',
 		description: 'Marble cross tombstone.',
-		price: 7900,
+		price: 27000,
 		image: '/images/products/KT035.jpg'
 	},
 	{
 		id: 'KT036',
 		name: 'KT036',
 		description: 'Black granite family tombstone.',
-		price: 8000,
+		price: 16000,
 		image: '/images/products/KT036.jpg'
 	},
 	{
 		id: 'KT037',
 		name: 'KT037',
 		description: 'Elegant engraved upright.',
-		price: 8100,
+		price: 35000,
 		image: '/images/products/KT037.jpg'
 	},
 	{
 		id: 'KT038',
 		name: 'KT038',
 		description: 'Classic memorial tombstone.',
-		price: 8200,
+		price: 50000,
 		image: '/images/products/KT038.jpg'
 	},
 	{
 		id: 'KT039',
 		name: 'KT039',
 		description: 'Polished family memorial.',
-		price: 8300,
+		price: 75000,
 		image: '/images/products/KT039.jpg'
 	},
 	{
 		id: 'KT040',
 		name: 'KT040',
 		description: 'Heart-shaped engraved tombstone.',
-		price: 8400,
+		price: 40000,
 		image: '/images/products/KT040.jpg'
 	},
 	{
 		id: 'KT041',
 		name: 'KT041',
 		description: 'Classic upright memorial.',
-		price: 8500,
+		price: 12000,
 		image: '/images/products/KT041.jpg'
 	},
 	{
 		id: 'KT042',
 		name: 'KT042',
 		description: 'Marble tombstone with engraving.',
-		price: 8600,
+		price: 13000,
 		image: '/images/products/KT042.jpg'
 	},
 	{
 		id: 'KT043',
 		name: 'KT043',
 		description: 'Black granite elegant tombstone.',
-		price: 8700,
+		price: 7000,
 		image: '/images/products/KT043.jpg'
 	},
 	{
 		id: 'KT044',
 		name: 'KT044',
 		description: 'Family memorial upright.',
-		price: 8800,
+		price: 40000,
 		image: '/images/products/KT044.jpg'
 	},
 	{
 		id: 'KT045',
 		name: 'KT045',
 		description: 'Polished heart-shaped tombstone.',
-		price: 8900,
+		price: 55000,
 		image: '/images/products/KT045.jpg'
 	},
 	{
 		id: 'KT046',
 		name: 'KT046',
 		description: 'Classic granite headstone.',
-		price: 9000,
+		price: 35000,
 		image: '/images/products/KT046.jpg'
 	},
 	{
 		id: 'KT047',
 		name: 'KT047',
 		description: 'Marble memorial tombstone.',
-		price: 9100,
+		price: 8000,
 		image: '/images/products/KT047.jpg'
 	},
 	{
 		id: 'KT048',
 		name: 'KT048',
 		description: 'Black granite engraved tombstone.',
-		price: 9200,
+		price: 8000,
 		image: '/images/products/KT048.jpg'
 	},
 	{
 		id: 'KT049',
 		name: 'KT049',
 		description: 'Elegant family memorial.',
-		price: 9300,
+		price: 40000,
 		image: '/images/products/KT049.jpg'
 	},
 	{
 		id: 'KT050',
 		name: 'KT050',
 		description: 'Polished upright tombstone.',
-		price: 9400,
+		price: 45000,
 		image: '/images/products/KT050.jpg'
 	},
 	{
 		id: 'KT051',
 		name: 'KT051',
 		description: 'Heart-shaped granite memorial.',
-		price: 9500,
+		price: 15000,
 		image: '/images/products/KT051.jpg'
 	},
 	{
 		id: 'KT052',
 		name: 'KT052',
 		description: 'Classic engraved tombstone.',
-		price: 9600,
+		price: 40000,
 		image: '/images/products/KT052.jpg'
 	},
 	{
 		id: 'KT053',
 		name: 'KT053',
 		description: 'Marble family tombstone.',
-		price: 9700,
+		price: 40000,
 		image: '/images/products/KT053.jpg'
 	},
 	{
 		id: 'KT054',
 		name: 'KT054',
 		description: 'Black granite upright memorial.',
-		price: 9800,
+		price: 40000,
 		image: '/images/products/KT054.jpg'
 	},
 	{
 		id: 'KT055',
 		name: 'KT055',
 		description: 'Elegant heart-shaped tombstone.',
-		price: 9900,
+		price: 85000,
 		image: '/images/products/KT055.jpg'
 	},
 	{
 		id: 'KT056',
 		name: 'KT056',
 		description: 'Classic granite memorial.',
-		price: 10000,
+		price: 90000,
 		image: '/images/products/KT056.jpg'
 	},
 	{
 		id: 'KT057',
 		name: 'KT057',
 		description: 'Marble upright tombstone.',
-		price: 10100,
+		price: 85000,
 		image: '/images/products/KT057.jpg'
 	},
 	{
 		id: 'KT058',
 		name: 'KT058',
 		description: 'Black granite double memorial.',
-		price: 10200,
+		price: 40000,
 		image: '/images/products/KT058.jpg'
 	},
 	{
 		id: 'KT059',
 		name: 'KT059',
 		description: 'Family engraved tombstone.',
-		price: 10300,
+		price: 40000,
 		image: '/images/products/KT059.jpg'
 	},
 	{
 		id: 'KT060',
 		name: 'KT060',
 		description: 'Polished upright memorial.',
-		price: 10400,
+		price: 35000,
 		image: '/images/products/KT060.jpg'
 	},
 	{
 		id: 'KT061',
 		name: 'KT061',
 		description: 'Heart-shaped granite tombstone.',
-		price: 10500,
+		price: 40000,
 		image: '/images/products/KT061.jpg'
 	},
 	{
 		id: 'KT062',
 		name: 'KT062',
 		description: 'Classic family memorial.',
-		price: 10600,
+		price: 60000,
 		image: '/images/products/KT062.jpg'
 	},
 	{
 		id: 'KT063',
 		name: 'KT063',
 		description: 'Elegant upright tombstone.',
-		price: 10700,
+		price: 65000,
 		image: '/images/products/KT063.jpg'
 	}
 ];
